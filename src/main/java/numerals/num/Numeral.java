e3fc72c6bfc786b1d6b6e87a2947c760ee6a2b62
lostguardcs <emilymgosselin@gmail.com>
2025-04-30 23:51:13 +0000

    code: convert 10 to X

--- src/main/java/numerals/num/Numeral.java
+++ src/main/java/numerals/num/Numeral.java
@@ -3,8 +3,9 @@ package numerals.num;
 public class Numeral {
 
 	String convert(int givenNum){
-
-        if (givenNum == 5){
+        if (givenNum == 10) {
+            return "X";
+        }  else if (givenNum == 5){
             return "V";
         } else {
             return "I";
