09e54decc11cc218594e950dc6692e355c98b068
lostguardcs <emilymgosselin@gmail.com>
2025-04-30 23:47:04 +0000

    clean: create setup to prevent repetition

--- src/test/java/numerals/num/NumeralTests.java
+++ src/test/java/numerals/num/NumeralTests.java
@@ -15,10 +15,16 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.api.BeforeEach;
 
 class NumeralTests {
 
+    private Numeral n;
+    @BeforeEach
+    void setup(){
+        n = new Numeral();
+    }
+    
     @Test
     void testOne(){
         Numeral n = new Numeral();
