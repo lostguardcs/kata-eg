e9b8753a73ce0fe290c5a6861f21b75f4acb39b9
lostguardcs <emilymgosselin@gmail.com>
2025-05-01 00:36:31 +0000

    code: convert 2000 into MM

--- src/main/java/numerals/num/Numeral.java
+++ src/main/java/numerals/num/Numeral.java
@@ -3,21 +3,34 @@ package numerals.num;
 public class Numeral {
 
 	String convert(int givenNum){
-        if (givenNum == 1000) {
-            return "M";
-        } else if (givenNum == 500){
-            return "D";
-        } else if (givenNum == 100){
-            return "C";
-        } else if (givenNum == 50) {
-            return "L";
-        } else if (givenNum == 10) {
-            return "X";
-        }  else if (givenNum == 5){
-            return "V";
-        } else {
-            return "I";
+        int remaining = givenNum;
+        String result = "";
+
+        while (remaining != 0){
+        if (remaining >= 1000) {
+            result += "M";
+            remaining = remaining - 1000;
+        } else if (givenNum >= 500){
+            result += "D";
+            remaining = remaining - 500;
+        } else if (givenNum >= 100){
+            result += "C";
+            remaining = remaining - 100;
+        } else if (givenNum >= 50) {
+            result += "L";
+            remaining = remaining - 50;
+        } else if (givenNum >= 10) {
+            result += "X";
+            remaining = remaining - 10;
+        }  else if (givenNum >= 5){
+            result += "V";
+            remaining = remaining - 5;
+        } else if (givenNum > 0) {
+            result += "I";
+            remaining = remaining - 1;
+        } 
         }
+        return result;
     }
 
 }
