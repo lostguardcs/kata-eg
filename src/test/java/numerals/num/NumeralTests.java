074a5c13c7e4c9308cbc53193b0f0702c4c56c49
lostguardcs <emilymgosselin@gmail.com>
2025-05-01 00:23:02 +0000

    clean: combine into one parameterized test

--- src/test/java/numerals/num/NumeralTests.java
+++ src/test/java/numerals/num/NumeralTests.java
@@ -15,6 +15,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.api.BeforeEach;
 
 class NumeralTests {
@@ -25,38 +26,9 @@ class NumeralTests {
         n = new Numeral();
     }
 
-    @Test
-    void testOne(){
-        assertEquals("I", n.convert(1));
-    }
-
-    @Test
-    void testFive(){
-        assertEquals("V", n.convert(5));
-    }
-
-    @Test
-    void testTen(){
-        assertEquals("X", n.convert(10));
-    }
-
-    @Test
-    void testFifty(){
-        assertEquals("L", n.convert(50));
-    }
-
-    @Test 
-    void testOneHundred(){
-        assertEquals("C", n.convert(100));
-    }
-
-    @Test
-    void testFiveHundred(){
-        assertEquals("D", n.convert(500));
-    }
-
-    @Test
-    void testOneThousand(){
-        assertEquals("M", n.convert(1000));
+    @ParameterizedTest
+    @CsvSource({"I, 1", "V, 5", "X, 10", "L, 50", "C, 100", "D, 500", "M, 1000"})
+    void testFundamentalNumerals(String result, int input){
+        assertEquals(result, n.convert(input));
     }
 }
